d019041a39c521d313c1590c3390893cc2dd9dce
Zachariah Brown <crazyzprogrammer@hotmail.com>
2014-09-17 11:13:55 -0400

    Added register thanks page.

---
+++ register/thanks/index.php
@@ -0,0 +1,11 @@
+<?php $Title = "Thanks for Registering!"; include_once $_SERVER['DOCUMENT_ROOT'].'/layout/header.php'; ?>
+
+<div class="row">
+  <div class="jumbotron">
+    <h2 class="text-center">Thanks for registering <?php echo $session->GetUsername(); ?>!</h2>
+    <br>
+    <h3 class="text-center">You are now free to move about the <a href="/account">holodeck</a>.</h3>
+  </div>
+</div>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/layout/footer.php'; ?>
