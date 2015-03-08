18bded3c2ef0bba8e2fc109797f41d2ac0ddea7d
Zachariah Brown <crazyzprogrammer@hotmail.com>
2015-03-07 23:14:13 -0500

    Added timer animation

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -12,3 +12,28 @@ function DotAnimation(Element, AnimatedText) {
     else Element.append('.');
   }, 500);
 };
+
+function TimerAnimation(Element, EndTime) {
+  lastseconds = -1;
+  milliseconds = 1000;
+  interval = 28;
+
+  return window.setInterval(function() {
+    difference = Math.max(0, moment.unix(EndTime - moment().unix()).unix());
+    days = Math.floor(difference / 86400);
+    difference -= days * 86400;
+    hours = Math.floor(difference / 3600);
+    difference -= hours * 3600;
+    minutes = Math.floor(difference / 60);
+    seconds = difference - minutes * 60;
+
+   if (lastseconds != seconds && seconds != 0) {
+     lastseconds = seconds;
+     milliseconds = 1000;
+   }
+   else if (seconds == 0) milliseconds = 0;
+   else milliseconds = Math.max(0, milliseconds - interval);
+
+    Element.html(days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ' + milliseconds + 'ms');
+  }, interval);
+};
