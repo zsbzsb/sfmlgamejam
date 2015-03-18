71d60a7410061e712dd0acd7d33ecab1a250b315
Zachariah Brown <crazyzprogrammer@hotmail.com>
2015-03-18 08:55:54 -0400

    Improved the jam table list and footer

--- content/templates/footer.php
+++ content/templates/footer.php
@@ -1,7 +1,7 @@
     </div>
     <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation" id="footer">
       <div class="container">
-        <div class="navbar">
+        <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-left">
             <li><a href="<?= $routes->generate('terms') ?>">Terms of Use</a></li>
             <li><a href="https://www.github.com/zsbzsb/sfmlgamejam/" target="_blank">Source Code</a></li>
--- content/views/jams_list.php
+++ content/views/jams_list.php
@@ -4,37 +4,32 @@
 
 <div class="row">
   <div class="col-md-8 col-md-offset-2">
-    <div class="panel panel-default">
-      <div class="panel-body">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Suggestions Open</th>
-              <th>Jam Start</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Suggestions Open</th>
+          <th>Jam Start</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
 
-              foreach ($jams as $jam)
-              {
-                echo '
-                  <tr class="jamrow" jamviewurl="'.$routes->generate('jam_page', array('id' => $jam['id'])).'">
-                    <td>'.$jam['title'].'</td>
-                    <td>'.date($DATETIME_FORMAT, $jam['suggestionsbegin']).'</td>
-                    <td>'.date($DATETIME_FORMAT, $jam['suggestionsbegin'] + $jam['suggestionslength'] + $jam['approvallength'] + $jam['themeannouncelength']).'</td>
-                    <td>'.JamStatusString($jam['status']).'</td>
-                  </tr>';
-              }
-
-            ?>
-          </tbody>
-        </table>
-        <?php if (count($jams) == 0) echo 'Nothing was found :('; ?>
-      </div>
-    </div>
+          foreach ($jams as $jam)
+          {
+            echo '
+              <tr class="jamrow" jamviewurl="'.$routes->generate('jam_page', array('id' => $jam['id'])).'">
+                <td>'.$jam['title'].'</td>
+                <td>'.date($DATETIME_FORMAT, $jam['suggestionsbegin']).'</td>
+                <td>'.date($DATETIME_FORMAT, $jam['suggestionsbegin'] + $jam['suggestionslength'] + $jam['approvallength'] + $jam['themeannouncelength']).'</td>
+                <td>'.JamStatusString($jam['status']).'</td>
+              </tr>';
+          }
+        ?>
+      </tbody>
+    </table>
+    <?php if (count($jams) == 0) echo 'Nothing was found :('; ?>
   </div>
 </div>
 
