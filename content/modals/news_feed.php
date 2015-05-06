85be862e648ed552d0992ebd3559a87d3613b983
Zachariah Brown <crazyzprogrammer@hotmail.com>
2015-05-05 21:16:51 -0400

    Fixed news feed ordering

--- content/modals/news_feed.php
+++ content/modals/news_feed.php
@@ -3,7 +3,7 @@
 $news = $cache->get('news_feed_news');
 if ($news == null)
 {
-  $stmt = $dbconnection->prepare('SELECT id, title, date, summary FROM news WHERE date <= ? ORDER BY date;');
+  $stmt = $dbconnection->prepare('SELECT id, title, date, summary FROM news WHERE date <= ? ORDER BY date DESC;');
   $stmt->execute(array(time()));
   $news = $stmt->fetchAll();
 
