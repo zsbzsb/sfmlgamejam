479a1534c9dffb6bb644e716ebccc77f37866887
Zachariah Brown <crazyzprogrammer@hotmail.com>
2015-05-08 09:43:53 -0400

    Added blank target to timezone link

--- content/views/time.php
+++ content/views/time.php
@@ -2,7 +2,7 @@
   <div class="jumbotron">
     <h2 class="text-center" id="timebigdisplay"></h2>
     <br>
-    <h5 class="text-center">Server time is <a href="http://en.wikipedia.org/wiki/UTC%C2%B100:00">UTC&#177;00:00</a></h5>
+    <h5 class="text-center">Server time is <a href="http://en.wikipedia.org/wiki/UTC%C2%B100:00" target="_blank">UTC&#177;00:00</a></h5>
   </div>
 </div>
 
