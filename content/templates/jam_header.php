3fad16a852db3da342b5d62dae0260f810b6c2d5
Zachariah Brown <crazyzprogrammer@hotmail.com>
2015-05-15 11:34:43 -0400

    Adjusted font in jam header

--- content/templates/jam_header.php
+++ content/templates/jam_header.php
@@ -1,7 +1,7 @@
 <div class="row">
-  <h2 class="text-center"><a href="<?=$routes->generate('jam_page', array('id' => $id))?>"><?=$jam['title']?></a></h2>
+  <h2 class="text-center"><strong><a href="<?=$routes->generate('jam_page', array('id' => $id))?>"><?=$jam['title']?></a></strong></h2>
   <h4 class="text-center text-muted"><?= date(DATE_FORMAT, JamBegins($jam)).' to '.date(DATE_FORMAT, SubmissionsBegin($jam)); ?></h4>
-  <?php if (isset($jam['selectedtheme'])) echo '<h2 class="text-center text-warning aftertag" aftertag="is the chosen theme">\''.$jam['selectedtheme'].'\'</h2>'; ?>
+  <?php if (isset($jam['selectedtheme'])) echo '<h2 class="text-center text-warning aftertag" aftertag="is the chosen theme">\'<i>'.$jam['selectedtheme'].'</i>\'</h2>'; ?>
   <?php require TEMPLATEROOT.'countdown.php'; ?>
   <hr />
 </div>
