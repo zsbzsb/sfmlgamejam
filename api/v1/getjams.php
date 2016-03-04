5145fcd3a8e8cedf90cff1c1c61b3e47d5eca9c3
Zachariah Brown <contact@zbrown.net>
2016-03-04 14:55:12 -0500

    Updated api/v1/jams to return more sensible data

--- api/v1/getjams.php
+++ api/v1/getjams.php
@@ -1,9 +1,31 @@
 <?php
 
-$stmt = $dbconnection->prepare('SELECT id, title, suggestionsbegin, suggestionslength, approvallength, themeannouncelength, jamlength, submissionslength, judginglength, status, currentround FROM jams ORDER BY suggestionsbegin DESC;');
+require SCRIPTROOT.'jamstates.php';
+
+$stmt = $dbconnection->prepare('SELECT * FROM jams ORDER BY suggestionsbegin DESC;');
 $stmt->execute();
 
 $rows = $stmt->fetchAll();
-SendResponse($rows);
+$jams = array();
+
+foreach ($rows as $row)
+{
+  $jam = array();
+
+  $jam['id'] = $row['id'];
+  $jam['title'] = $row['title'];
+  $jam['status'] = $row['status'];
+  $jam['suggestionsbegin'] = SuggestionsBegin($row);
+  $jam['votingbegins'] = VotingBegins($row);
+  $jam['themeannounce'] = ThemeAnnounce($row);
+  $jam['jambegins'] = JamBegins($row);
+  $jam['submissionsbegin'] = SubmissionsBegin($row);
+  $jam['submissionsend'] = SubmissionsEnd($row);
+  $jam['judgingends'] = JudgingEnds($row);
+
+  array_push($jams, $jam);
+}
+
+SendResponse($jams);
 
 ?>
